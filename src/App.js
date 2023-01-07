b89d2c4f83ad640f10d5165b8d50f4fba00a8bd4
Edmond Kadriu <66006624+edmondkadriu@users.noreply.github.com>
2023-01-08 00:16:36 +0100

    dataDisplayInGrid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import Launch from "./Launch";
 import './App.css';
 
-function App() {
+export function App() {
+  
+  const url ="https://api.spacexdata.com/v3/launches";
+  const [popular, setPopular] = useState([]);
+  useEffect(() => { fetchPopular();},[]);
+
+  const fetchPopular = async () => {
+    const data = await fetch(url);
+    const launches = await data.json();
+    console.log(launches);
+    setPopular(launches);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Launches</h1>
+      <div className="popular-launches">
+      {popular.map((launch) => {
+          return <Launch key={launch.mission_name} launch={launch} />;
+        })}
+      </div>
     </div>
   );
 }
